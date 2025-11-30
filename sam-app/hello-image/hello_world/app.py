84e45110e443e73e7a7a532ca651dfe2f30c13a7
blake-travers <btra0031@student.monash.edu>
2025-11-30 17:20:06 +1100

    Moving back to windows

--- sam-app/hello-image/hello_world/app.py
+++ sam-app/hello-image/hello_world/app.py
@@ -1,8 +1,11 @@
-from osgeo import gdal
-
+import json
+import rasterio
 
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": gdal.VersionInfo()
+        "body": json.dumps({
+            "rasterio": rasterio.__version__,
+            "gdal": rasterio.__gdal_version__
+        })
     }
